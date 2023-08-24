53f30425adc8368de31d260cc9392f2138fa8bef
Erwan <erwan-14730@outlook.fr>
2023-08-24 13:53:44 +0200

    Mise en place du login avec fetch et la method post, il reste des elements manquant

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,5 +1,3 @@
-// email: sophie.bluel@test.tld
-// password: S0phie
 const loginEmail = "sophie.bluel@test.tld";
 const loginPassword = "S0phie";
 
@@ -7,17 +5,34 @@ const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
 const loginForm = document.querySelector(".login-form");
 
-loginForm.addEventListener("submit", (e) => {
+loginForm.addEventListener("submit", async (e) => {
   e.preventDefault();
   const enteredEmail = emailInput.value;
   const enteredPassword = passwordInput.value;
 
-  if ((enteredEmail === loginEmail) & (enteredPassword === loginPassword)) {
-    alert("Authentification réussi !");
-  } else if (
-    (enteredEmail !== loginEmail) &
-    (enteredPassword !== loginPassword)
-  ) {
-    alert("Email ou mot de passe incorrect");
+  const AuthentificationData = {
+    email: enteredEmail,
+    password: enteredPassword,
+  };
+
+  try {
+    const response = await fetch("http://localhost:5678/api/users/login", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(AuthentificationData),
+    });
+
+    if (response.ok) {
+      //Auth réussi
+      alert("Authentification réussi !");
+      window.location.href = "http://127.0.0.1:5500/FrontEnd/index.html";
+    } else {
+      // Auth Echoué
+      alert("Email ou mot de passe incorrect");
+    }
+  } catch (error) {
+    console.error("Erreur lors de l'authentification :", error);
   }
 });
