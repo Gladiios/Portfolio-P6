e4544ec67ba5475b30385abdc5d1762a5dc1093b
Erwan <erwan-14730@outlook.fr>
2023-08-28 10:48:39 +0200

    suite

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -1,10 +1,10 @@
 const modalContainer = document.querySelector(".modal-container");
 const modalTriggers = document.querySelectorAll(".modal-trigger");
 
-console.log(modalTriggers);
+modalTriggers.forEach((trigger) => {
+  trigger.addEventListener("click", toggleModal);
+});
 
-modalTriggers.forEach((trigger) =>
-  trigger.addEventListener("click", () => {
-    console.log("click", trigger);
-  })
-);
+function toggleModal() {
+  modalContainer.classList.toggle("active");
+}
