4441111ddd078d0e7eed3642d1fcdcbe844a91eb
Erwan <erwan-14730@outlook.fr>
2023-08-28 16:12:32 +0200

    Ajout du contenu dans la modale, images des projets, texte editer et icone de corbeille

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -1,10 +1,35 @@
 const modalContainer = document.querySelector(".modal-container");
 const modalTriggers = document.querySelectorAll(".modal-trigger");
+const modalGallery = document.querySelector(".modal-gallery");
+
+let modalImages = [];
+
+function toggleModal() {
+  modalContainer.classList.toggle("active");
+}
 
 modalTriggers.forEach((trigger) => {
   trigger.addEventListener("click", toggleModal);
 });
 
-function toggleModal() {
-  modalContainer.classList.toggle("active");
+async function filterProjects() {
+  await fetch("http://localhost:5678/api/works")
+    .then((res) => res.json())
+    .then((data) => (modalImages = data));
+
+  modalGallery.innerHTML = modalImages
+    .map(
+      (modalContent) =>
+        `
+       <div class = "modal-gallery-content">
+        <div class="image-container">
+          <img src=${modalContent.imageUrl} alt=${modalContent.text}>
+          <i class="fa-solid fa-trash-can"></i>
+        </div>
+          <p class="edit">éditer</p>
+       </div>
+       `
+    )
+    .join("");
 }
+filterProjects();
